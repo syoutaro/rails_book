97686681b2c3a9642144c7fde1e2ab1d6d75bcfd
sugawara <pgjsugawara258@icloud.com>
2018-11-07 09:54:23 +0900

    Add devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    APP_NAME = "Rails_Board".freeze
+
+    def page_title
+      base_title = APP_NAME
+      return base_title if @title.blank?
+      "#{base_title} | #{@title}"
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,18 @@ module RailsBook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.skip_routes true
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
+
+    config.time_zone = 'Tokyo'
   end
 end
